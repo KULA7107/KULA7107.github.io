5ab6700f08e263cc393f070073e9e40578cd0d6b
KULA7107 <teddybear0259@gmail.com>
2023-11-23 04:43:04 -0500

    [update] index.html

--- myscripts.js
+++ myscripts.js
@@ -1,14 +1,16 @@
-// const element = document.getElementById("flow-box");
-// $(document).on('ready', function() {
-	for(let i=0;i<6;i++) {
-		document.getElementById("flow-box").innerHTML += 
-		`<div class="grid-colum">
-				<div class="title-box green space-${i}x">
-					<p>This is box${i}</p>
+let length = 7;
+for (let i = 0; i < length; i++) {
+  console.log(i);
+  document.getElementById(
+    "flow-box"
+  ).innerHTML += `<div class="grid-colum" style="grid-template-columns: ${Array(
+    length + 3
+  )
+    .fill(0)
+    .reduce((accumulator, currentValue) => accumulator + " auto", "")}">
+				<div class="title-box green" style="grid-column: ${i + 2} / span 2">
+					<p>This is box${i + 1}</p>
 				</div>
 			</div>
-		`;	
-
-	}
-
-// })
+		`;
+}
